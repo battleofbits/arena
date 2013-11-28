86da1f2d04a0090d89c715f9342c91a17f5a81bb
Kevin Burke <kevin@twilio.com>
2013-11-28 14:17:50 -0800

    wip. postgres go driver doesn't support arrays

--- arena/game.go
+++ arena/game.go
@@ -7,8 +7,8 @@ import (
 
 func CreatePlayer(username string, name string) (int64, *pq.Error) {
 	db := getConnection()
-	result, err := db.Exec("INSERT INTO players (username, name) VALUES ($1, $2) RETURNING id", username, name)
-	fmt.Println(result)
+	var id int64
+	_, err := db.Exec("INSERT INTO players (username, name) VALUES ($1, $2) RETURNING id", username, name)
 	fmt.Println("after insert")
 	var pqerr *pq.Error
 	if err != nil {
@@ -18,11 +18,62 @@ func CreatePlayer(username string, name string) (int64, *pq.Error) {
 		return -1, pqerr
 	}
 	checkError(err)
-	return result, nil
+	return id, nil
 }
 
-func CreateGame() {
-	fmt.Println("creating game")
+type Player struct {
+	// The player's unique Id
+	Name string
+	// The player's friendly name
+	Username string
+	// The autoid for the player
+	Id int64
+}
+
+func GetPlayerByName(name string) (*Player, error) {
+	var p Player
+	db := getConnection()
+	err := db.QueryRow("SELECT * FROM players WHERE name = $1", name).Scan(&p.Username, &p.Name, &p.Id)
+	fmt.Println(p.Username)
+	if err != nil {
+		return &Player{}, err
+	} else {
+		return &p, nil
+	}
+}
+
+type FourUpMatch struct {
+	RedPlayerId   int64
+	BlackPlayerId int64
+	Winner        int64
+	Board         [7][7]int
+}
+
+func InitializeBoard() [7][7]int {
+	var board [7][7]int
+	// Board is initialized to be filled with zeros.
+	return board
+}
+
+func CreateFourUpMatch(redPlayerName string, blackPlayerName string) (*FourUpMatch, error) {
+	redPlayer, err := GetPlayerByName(redPlayerName)
+	if err != nil {
+		return &FourUpMatch{}, err
+	}
+	blackPlayer, err := GetPlayerByName(blackPlayerName)
+	if err != nil {
+		return &FourUpMatch{}, err
+	}
+	board := InitializeBoard()
+	db := getConnection()
+	_, err = db.Exec("INSERT INTO fourup_matches (player_red, player_black) VALUES ($1, $2) RETURNING id", redPlayer.Id, blackPlayer.Id)
+	checkError(err)
+	fmt.Println("returning match")
+	return &FourUpMatch{
+		RedPlayerId:   redPlayer.Id,
+		BlackPlayerId: blackPlayer.Id,
+		Board:         board,
+	}, nil
 }
 
 const URL = "http://localhost:5000"
@@ -36,6 +87,8 @@ func main() {
 	if err != nil && err.Code.Name() != "unique_violation" {
 		checkError(err)
 	}
-	CreateGame()
+	match, fourupErr := CreateFourUpMatch("kevinburke", "kyleconroy")
+	checkError(fourupErr)
+	fmt.Println(match.Board)
 	fmt.Println("done")
 }
