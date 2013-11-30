718115ec87f54c12195fb8b415dd21825e417cb7
Kevin Burke <kevin@twilio.com>
2013-11-30 14:08:02 -0800

    break out match functions into their own class

--- game.go
+++ game.go
@@ -1,3 +1,4 @@
+// Contains the base logic for a game, integrates all of the models etc.
 package arena
 
 import (
@@ -14,16 +15,6 @@ const BaseUri = "https://battleofbits.com"
 const Empty = 0
 const Red = 1
 const Black = 2
-
-type FourUpMatch struct {
-	Id          int64
-	RedPlayer   *Player
-	BlackPlayer *Player
-	// Whose turn is it
-	CurrentPlayer *Player
-	Winner        int64
-	Board         *[NumRows][NumColumns]int
-}
 
 type TurnPlayers struct {
 	Red   string `json:"R"`
@@ -57,48 +48,6 @@ func serializeTurn(match *FourUpMatch) *FourUpTurn {
 		},
 	}
 }
-
-func CreateFourUpMatch(redPlayer *Player, blackPlayer *Player) *FourUpMatch {
-	board := InitializeBoard()
-	match := &FourUpMatch{
-		RedPlayer:   redPlayer,
-		BlackPlayer: blackPlayer,
-		Board:       board,
-		// Red plays first, I believe.
-		CurrentPlayer: redPlayer,
-	}
-	return match
-}
-
-func WriteMatch(match *FourUpMatch) error {
-	db := getConnection()
-	defer db.Close()
-	stringBoard := GetStringBoard(match.Board)
-	jsonBoard, err := json.Marshal(stringBoard)
-	if err != nil {
-		return err
-	}
-	query := "INSERT INTO fourup_matches " +
-		"(player_red, player_black, board, started) VALUES " +
-		"($1, $2, $3, NOW() at time zone 'utc') RETURNING id"
-	return db.QueryRow(query, match.RedPlayer.Id, match.BlackPlayer.Id, string(jsonBoard)).Scan(&match.Id)
-}
-
-// Update the match in the database
-// Assumes the match has been initialized at some point
-func UpdateMatch(match *FourUpMatch) error {
-	db := getConnection()
-	defer db.Close()
-	stringBoard := GetStringBoard(match.Board)
-	jsonBoard, err := json.Marshal(stringBoard)
-	if err != nil {
-		return err
-	}
-	query := "UPDATE fourup_matches SET board = $1 WHERE id = $2"
-	_, err = db.Exec(query, string(jsonBoard), match.Id)
-	return err
-}
-
 func DoForfeit(loser *Player, reason error) {
 	fmt.Println(fmt.Sprintf("player %s forfeits because of %s", loser.Username, reason.Error()))
 }
@@ -108,10 +57,6 @@ func DoGameOver(match *FourUpMatch, winner *Player, loser *Player) {
 	MarkWinner(match, winner)
 	NotifyWinner(winner)
 	NotifyLoser(loser)
-}
-
-func getMatchHref(matchId int64) string {
-	return fmt.Sprintf(BaseUri+"/games/four-up/matches/%d", matchId)
 }
 
 // Assemble and make an HTTP request to the user's URL
---
+++ match.go
@@ -0,0 +1,62 @@
+// Data layer for a match
+package arena
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type FourUpMatch struct {
+	Id          int64
+	RedPlayer   *Player
+	BlackPlayer *Player
+	// Whose turn is it
+	CurrentPlayer *Player
+	Winner        int64
+	Board         *[NumRows][NumColumns]int
+}
+
+func getMatchHref(matchId int64) string {
+	return fmt.Sprintf(BaseUri+"/games/four-up/matches/%d", matchId)
+}
+
+func CreateFourUpMatch(redPlayer *Player, blackPlayer *Player) *FourUpMatch {
+	board := InitializeBoard()
+	match := &FourUpMatch{
+		RedPlayer:   redPlayer,
+		BlackPlayer: blackPlayer,
+		Board:       board,
+		// Red plays first, I believe.
+		CurrentPlayer: redPlayer,
+	}
+	return match
+}
+
+func WriteMatch(match *FourUpMatch) error {
+	db := getConnection()
+	defer db.Close()
+	stringBoard := GetStringBoard(match.Board)
+	jsonBoard, err := json.Marshal(stringBoard)
+	if err != nil {
+		return err
+	}
+	query := "INSERT INTO fourup_matches " +
+		"(player_red, player_black, board, started) VALUES " +
+		"($1, $2, $3, NOW() at time zone 'utc') RETURNING id"
+	return db.QueryRow(query, match.RedPlayer.Id, match.BlackPlayer.Id, string(jsonBoard)).Scan(&match.Id)
+}
+
+// Update the match in the database
+// Assumes the match has been initialized at some point
+func UpdateMatch(match *FourUpMatch) error {
+	db := getConnection()
+	defer db.Close()
+	stringBoard := GetStringBoard(match.Board)
+	jsonBoard, err := json.Marshal(stringBoard)
+	if err != nil {
+		return err
+	}
+	query := "UPDATE fourup_matches SET board = $1 WHERE id = $2"
+	_, err = db.Exec(query, string(jsonBoard), match.Id)
+	return err
+}
