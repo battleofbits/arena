a25cf0c6f15384601a03dfa437b98ae13e0b0ece
Kyle Conroy <kyle@kyleconroy.com>
2014-01-16 21:05:36 -0800

    Add the engine

---
+++ engine/engine.go
@@ -0,0 +1,90 @@
+package engine
+
+import (
+	"io/ioutil"
+)
+
+const USER_AGENT = "battleofbits/0.1"
+
+type Match interface {
+	CurrentPlayer() Player
+}
+
+// The player's friendly name
+type Player struct {
+	Id int64 `json:"-"`
+	Name string `json:"name"`
+	Username  string `json:"username"`
+	MatchUrl  string `json:"-"`
+	Href      string `json:"href"`
+}
+
+
+func GetMove(player *Player, match *Match) {
+}
+
+
+func MakeRequest(url string, body []byte) (*http.Response, error) {
+	req, err := http.NewRequest("POST", url, bytes.NewReader(postBody))
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("User-Agent", USER_AGENT)
+	// XXX, set a timeout here
+	return client.Do(req)
+}
+
+// Assemble and make an HTTP request to the user's URL
+// Returns the column of the response
+func GetMove(match *Match) ([]byte, error) {
+	payload, err := json.Marshal(match)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	player := match.CurrentPlayer()
+
+	response, err := MakeRequest(player.MatchUrl, payload)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return body
+}
+
+func PlayMatch(match *Match, players []Player) {
+    for {
+        player := m.CurrentPlayer()
+        
+        move, err := GetMove(player, m)
+        
+        if err != nil {
+            m.Forfeit(player)
+        }
+        
+        gameover, err := m.Play(player, move)
+        
+        if err != nil {
+            //Move was invalid, game is over
+        }
+
+        if gameover {
+            // Record the winner
+            winner, err := m.Winner()
+        }
+    }
+}
