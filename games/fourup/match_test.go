befd5ad94c84990646dfb1f483c3a839bd65ec04
Kevin Burke <kevin@twilio.com>
2014-01-16 23:29:00 -0800

    rudimentary (failing) test and other things

---
+++ games/fourup/match_test.go
@@ -0,0 +1,29 @@
+package fourup
+
+import (
+	"fmt"
+	"github.com/battleofbits/arena/engine"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGameLogic(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "{\"column\": %d}", rand.Intn(NumColumns))
+	}))
+
+	players := []*engine.Player{
+		&engine.Player{Id: 1, Name: "kevinburke", MatchUrl: ts.URL},
+		&engine.Player{Id: 2, Name: "deferman", MatchUrl: ts.URL},
+	}
+	match, err := CreateMatch(players)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	err = engine.PlayMatch(match)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+}
