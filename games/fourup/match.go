9b00c8d0addbdae6674606def25fd10be736cec0
Kyle Conroy <kyle@kyleconroy.com>
2014-01-16 22:46:49 -0800

    All tests pass

--- games/fourup/match.go
+++ games/fourup/match.go
@@ -4,107 +4,46 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/battleofbits/arena/arena"
+	"github.com/battleofbits/arena/engine"
 	"time"
 )
 
 // This level of indirection necessary to translate between int/string
 // representation. Maybe we should just store everything as strings.
-type FourUpBoard struct {
-	Board [NumRows][NumColumns]int8
-}
-
-type FourUpMatch struct {
-	Players       []*arena.Player
+type Match struct {
+	Players       []*engine.Player
 	Started       time.Time
-	Board         *FourUpBoard
-	currentPlayer *arena.Player
+	Board         *Board
+	currentPlayer *engine.Player
 	MoveId        int64
-	winner        *arena.Player
+	winner        *engine.Player
 	Id            int64
 }
 
-func getStringBoard(board [NumRows][NumColumns]int8) [NumRows][NumColumns]string {
-	var stringBoard [NumRows][NumColumns]string
-	for row := int8(0); row < NumRows; row++ {
-		for column := int8(0); column < NumColumns; column++ {
-			if board[row][column] == Empty {
-				stringBoard[row][column] = ""
-			} else if board[row][column] == Red {
-				stringBoard[row][column] = "R"
-			} else if board[row][column] == Black {
-				stringBoard[row][column] = "B"
-			} else {
-				panic(fmt.Sprint("invalid value ", board[row][column], " for a board"))
-			}
-		}
-	}
-	return stringBoard
-}
-
-// Leaving this here till we're sure we don't need it, the method below
-// replaces this one.
-//func serializeTurn(match *FourUpMatch) *FourUpTurn {
-//return &FourUpTurn{
-//Href:  getMatchHref(match.Id),
-//Board: GetStringBoard(match.Board),
-//Turn:  fmt.Sprintf(BaseUri+"/players/%s", match.CurrentPlayer.Name),
-//Players: &TurnPlayers{
-//Red:   fmt.Sprintf(BaseUri+"/players/%s", match.RedPlayer.Name),
-//Black: fmt.Sprintf(BaseUri+"/players/%s", match.BlackPlayer.Name),
-//},
-//}
-//}
-
-func (b *FourUpBoard) MarshalJSON() ([]byte, error) {
-	sbd := getStringBoard(b.Board)
-	return json.Marshal(sbd)
+// A four up move
+type Move struct {
+	Column int8 `json:"column"`
 }
 
 // Retrieve the current player.
-func (m *FourUpMatch) CurrentPlayer() *arena.Player {
+func (m *Match) CurrentPlayer() *engine.Player {
 	return m.currentPlayer
 }
 
-func (m *FourUpMatch) Stalemate() bool {
+func (m *Match) Stalemate() bool {
 	// XXX
 	return false
 }
 
-func CreateMatch(players []*arena.Player) (FourUpMatch, error) {
+func CreateMatch(players []*engine.Player) (Match, error) {
 	if len(players) != 2 {
 		msg := fmt.Sprintf("wrong number of players: %d", len(players))
-		return FourUpMatch{}, errors.New(msg)
+		return Match{}, errors.New(msg)
 	}
 
-	return createFourUpMatch(players), nil
-}
+	var board *Board
 
-// Apply the move to the board, write it to the database
-// Returns a boolean (whether the game is over) and an error (whether the move
-// was invalid)
-func (m *FourUpMatch) Play(player *arena.Player, data []byte) (bool, error) {
-	var fm fourUpMove
-	err := json.Unmarshal(data, fm)
-	if err != nil {
-		return true, err
-	}
-	err = doNewMove(fm.Column, m)
-	if err != nil {
-		// XXX, assign the winner to be the other player.
-		m.winner = player
-		return true, err
-	}
-	if over, winner := gameOver(m.Board.Board); over {
-		m.winner = player
-		return true, nil
-	}
-	return false, nil
-}
-
-func createFourUpMatch(players []*arena.Player) *arena.Match {
-	var board *FourUpBoard
-	return &FourUpMatch{
+	match := Match{
 		Players: players,
 		Board:   board,
 		// Red plays first, I believe.
@@ -112,37 +51,41 @@ func createFourUpMatch(players []*arena.Player) *arena.Match {
 		MoveId:        0,
 		Started:       time.Now().UTC(),
 	}
+
+	return match, nil
 }
 
-func writeMatch(match *FourUpMatch) error {
-	db := arena.GetConnection()
-	defer db.Close()
-	jsonBoard, err := json.Marshal(match.Board)
+
+// Apply the move to the board, write it to the database
+// Returns a boolean (whether the game is over) and an error (whether the move
+// was invalid)
+func (m *Match) Play(player *engine.Player, data []byte) (bool, error) {
+	var move Move
+
+	err := json.Unmarshal(data, move)
+
 	if err != nil {
-		return err
+		return true, err
 	}
-	query := "INSERT INTO fourup_matches " +
-		"(player_red, player_black, board, started) VALUES " +
-		"($1, $2, $3, NOW() at time zone 'utc') RETURNING id"
-	return db.QueryRow(query, match.Players[0].Id, match.Players[1].Id,
-		string(jsonBoard)).Scan(&match.Id)
-}
 
-var moveWriter = writeMove
+	err = m.Board.ApplyMove(move.Column, m.getCurrentTurnColor())
 
-// Write a new move to the database
-func writeMove(move int8, match *arena.Match) (int64, error) {
-	db := arena.GetConnection()
-	defer db.Close()
-	query := "INSERT INTO fourup_moves (fourup_column, player, move_number, match_id, played)" +
-		"VALUES ($1, $2, $3, $4, NOW() at time zone 'utc') RETURNING id"
-	var moveId int64
-	err := db.QueryRow(query, move, match.CurrentPlayer.Id, match.MoveId, match.Id).Scan(&moveId)
-	return moveId, err
+	if err != nil {
+		// XXX, assign the winner to be the other player.
+		m.winner = player
+		return true, err
+	}
+
+	if over, _ := m.Board.GameOver(); over {
+		m.winner = player
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // Convert players => board color
-func (m *FourUpMatch) getCurrentTurnColor() int8 {
+func (m *Match) getCurrentTurnColor() int8 {
 	if m.CurrentPlayer() == m.Players[0] {
 		return Red
 	} else {
@@ -150,47 +93,4 @@ func (m *FourUpMatch) getCurrentTurnColor() int8 {
 	}
 }
 
-// A four up move
-type fourUpMove struct {
-	Column int8 `json:"column"`
-}
 
-// Do a whole bunch of stuff associated with new moves
-// Error handling is a little tricky because most of the errors would be
-// database or other errors.
-func doNewMove(column int8, match *arena.Match) error {
-	var err error
-	match.Board, err = applyMoveToBoard(column, match.GetCurrentTurnColor(),
-		match.Board)
-	// XXX
-	//if err != nil {
-	//DoForfeit(player, err)
-	//DoGameOver(match, otherPlayer, player)
-	//return err
-	//}
-	// once we know move was valid, update the database
-	_, err = writeMove(column, match)
-	if err != nil {
-		return err
-	}
-	match.MoveId++
-	err = UpdateMatch(match)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-// Update the match in the database
-// Assumes the match has been initialized at some point
-func UpdateMatch(match *FourUpMatch) error {
-	db := arena.GetConnection()
-	defer db.Close()
-	jsonBoard, err := json.Marshal(match.Board)
-	if err != nil {
-		return err
-	}
-	query := "UPDATE fourup_matches SET board = $1 WHERE id = $2"
-	_, err = db.Exec(query, string(jsonBoard), match.Id)
-	return err
-}
