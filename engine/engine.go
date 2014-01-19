7f39174c9bd269c66431c25b04f5fc324c7a7a76
Kevin Burke <kevin@twilio.com>
2014-01-18 17:22:01 -0800

    Add documentation

--- engine/engine.go
+++ engine/engine.go
@@ -33,12 +33,24 @@ func (p *Player) SetHref() {
 	p.Href = fmt.Sprintf("https://battleofbits.com/players/%s", p.Name)
 }
 
-// Every instance of a game should implement this interface
+// Every instance of a game must implement this interface
 type Match interface {
+
+	// Retrieve the player whose turn it is.
 	CurrentPlayer() *Player
+
+	// Serialize a move from the byte string and apply it to the board. Returns
+	// `true` if the game is over, and an error if the move was unreadable or
+	// invalid.
 	Play(*Player, []byte) (bool, error)
+
+	// Retrieve the winning player.
 	Winner() Player
+
+	// Determine whether the match is a stalemate.
 	Stalemate() bool
+
+	// Advance the turn.
 	NextPlayer() *Player
 }
 
